11a5c85877ac4dfa3e70871da3a487a620260049
guzno <anastasia.guznova@yandex.ru>
2021-12-04 11:29:02 +0300

    выполнено задание 4.4

---
+++ zadanie4/Form3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie4
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        private BindingSource sotrBindingSourse;
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            
+            // Загрузка таблицы данными:
+            товарыTableAdapter1.Fill(rBProductDataSet11.Товары);
+            // Создание BindingSource для таблицы Товары:
+            sotrBindingSourse = new BindingSource(rBProductDataSet11, "Товары");
+            // Настройка связывания для элементов TextBox:
+            NametextBox.DataBindings.Add("Text", sotrBindingSourse, "Наименование");
+            SectiontextBox.DataBindings.Add("Text", sotrBindingSourse, "Цена"); 
+
+        }
+
+        private void Previousbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MovePrevious();
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MoveNext();
+        }
+    }
+}
