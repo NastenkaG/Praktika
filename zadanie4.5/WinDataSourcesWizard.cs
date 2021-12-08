2797dd9ea51bf78e007b16ca045cb7dbe8cbcc73
guzno <anastasia.guznova@yandex.ru>
2021-12-08 16:57:17 +0300

    выполнено задание 4.5

---
+++ zadanie4.5/WinDataSourcesWizard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie4._5
+{
+    public partial class WinDataSourcesWizard : Form
+    {
+        public WinDataSourcesWizard()
+        {
+            InitializeComponent();
+        }
+
+        private void WinDataSourcesWizard_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rBProductDataSet.Товары". При необходимости она может быть перемещена или удалена.
+            this.товарыTableAdapter.Fill(this.rBProductDataSet.Товары);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rBProductDataSet.Товары". При необходимости она может быть перемещена или удалена.
+            this.товарыTableAdapter.Fill(this.rBProductDataSet.Товары);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rBProductDataSet.Товары". При необходимости она может быть перемещена или удалена.
+            this.товарыTableAdapter.Fill(this.rBProductDataSet.Товары);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rBProductDataSet.Товары". При необходимости она может быть перемещена или удалена.
+            this.товарыTableAdapter.Fill(this.rBProductDataSet.Товары);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rBProductDataSet.Поставщики". При необходимости она может быть перемещена или удалена.
+            this.поставщикиTableAdapter.Fill(this.rBProductDataSet.Поставщики);
+
+        }
+    }
+}
